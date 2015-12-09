2582a2ba1faf5a562b6b6a9b68f4e947a3dff39f
Matti Rita-Kasari <matti.rita-kasari@jubic.fi>
2015-12-09 17:34:18 +0200

    Comment out automatic configuration tests.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,21 +10,21 @@ export NSS_WRAPPER_GROUP=/etc/group
 if [ ! -d /var/www/drupal/sites/default ]; then
     cp -arf /tmp/sites/* /var/www/drupal/sites/
 
-    echo "Drupal database host: ${DRUPAL_SERVICE_NAME}-db"
-    echo "MySQL username:password: ${MYSQL_USER}:${MYSQL_PASSWORD}"
-    echo "Drupal site name: ${DRUPAL_SITE_NAME}"
+    #echo "Drupal database host: ${DRUPAL_SERVICE_NAME}-db"
+    #echo "MySQL username:password: ${MYSQL_USER}:${MYSQL_PASSWORD}"
+    #echo "Drupal site name: ${DRUPAL_SITE_NAME}"
 
-    sleep 30s
+    #sleep 30s
 
     # Wait until MySQL instance is running
-    while ! nc -z ${DRUPAL_SERVICE_NAME}-db 3306; do
-      sleep 1s
-    done
+    #while ! nc -z ${DRUPAL_SERVICE_NAME}-db 3306; do
+    #  sleep 1s
+    #done
 
-    cd /var/www/drupal
-    drush site-install -y standard \
-      --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/drupal \
-      --site-name="${DRUPAL_SITE_NAME}" --account-name=admin --account-pass=${MYSQL_PASSWORD}
+    #cd /var/www/drupal
+    #drush site-install -y standard \
+    #  --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${DRUPAL_SERVICE_NAME}-db/drupal \
+    #  --site-name="${DRUPAL_SITE_NAME}" --account-name=admin --account-pass=${MYSQL_PASSWORD}
 
     #cp -af /workdir/settings.php /var/www/drupal/sites/default/default.settings.php
 
